c60ba41fc17b4bc2e8f4ccc6e1c8320730243e6a
Ivison Rafael <irs3@cin.ufpe.br>
2025-03-27 21:04:23 +0000

    Motor de passos e mecânica de parar ativados

--- main.ino
+++ main.ino
@@ -1,34 +1,20 @@
-#define BUZZER1 3
-#define BUZZER2 2
-#define TRIG_PIN 9
-#define ECHO_PIN 10
+#include <Stepper.h>
+#define BUZZER 10
+#define TRIG_PIN 12
+#define ECHO_PIN 13
+#define STEPS 2048  // Passos por revolução do 28BYJ-48
 
-int controlar_tempo(int distancia){
-  if(distancia <= 12 && distancia > 10){
-    return 1000;
-  }
-  if(distancia <= 10 && distancia > 8){
-    return 800;
-  }
-  if(distancia <= 8 && distancia > 6){
-    return 500;
-  }
-  else{
-    return 200;
-  }
-}
-
+Stepper motor(STEPS, 2, 3, 4, 5);  // Pinos do driver no Arduino
 
 void setup() {
     Serial.begin(9600);
-    pinMode(BUZZER1, OUTPUT);
-    pinMode(BUZZER2, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
+    motor.setSpeed(10);  // Velocidade em RPM
 }
 
 void loop() {
-    bool isDelay = true;
     digitalWrite(TRIG_PIN, LOW);
     delayMicroseconds(2);
     digitalWrite(TRIG_PIN, HIGH);
@@ -37,22 +23,16 @@ void loop() {
 
     long duration = pulseIn(ECHO_PIN, HIGH);
     float distance = duration * 0.034 / 2; // Conversão para cm
-    if (distance <= 12.00 && distance >= 2.00){
+    if (distance <= 6.00 && distance > 1.00 ){
       Serial.println("Entrei");
-      int t = controlar_tempo(distance);
-      tone(BUZZER1, 300);
-      digitalWrite(BUZZER2, 1);
-      delay(t);
-      digitalWrite(BUZZER2, LOW);
-      noTone(BUZZER1);
-      isDelay = false;
+      tone(BUZZER, 7000);
     }
-   
+    else{
+      noTone(BUZZER);
+      motor.step(10);  
+    }
+
     Serial.print("Distância: ");
     Serial.print(distance);
     Serial.println(" cm");
-    if(isDelay){
-      delay(350);
-    }
-    isDelay = true;
 }
